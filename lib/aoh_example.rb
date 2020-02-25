79a40f40a6a832b4a87481bdf9ca6e009f36d8fb
Stephanie Zou <stephanie.zou@stern.nyu.edu>
2020-02-25 03:27:26 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -19,7 +19,15 @@ def assembled_aoh
 end
 
 def literal_aoh
+  characters = [
+    DON_G,
+    JOELLE_VD,
+    PAT_M,
+    KATE_G,
+    BRUCE_G
+  ]
   array = []
+  array = characters.sort
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
 end
